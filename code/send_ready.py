567cbad9075238ddb87feb4dbc8efd974501a075
Joakim Eriksson <90309369+aidotsejoakim@users.noreply.github.com>
2024-04-02 09:33:54 +0200

    Create send_ready.py

---
+++ code/send_ready.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import serial
+import time
+import sys
+import os
+
+
+def send_values_to_datalogger(message, ser_path):
+
+    message = str.encode(message) 
+    ser = serial.Serial(ser_path, 9600,timeout=(5),parity=serial.PARITY_NONE)           
+
+    ser.write(message) # Send message to datalogger
+    time.sleep(1)
+    ser.close()
+
+    return message
+
+
+if __name__ == '__main__':
+
+    ser_path = sys.argv[1]
+    send_values_to_datalogger('ready', ser_path)
