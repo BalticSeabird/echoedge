f739a41a27456b418d9d2c0ef4c882ec1ccf55da
hentatisundberg <jonas.sundberg@slu.se>
2024-04-14 15:32:51 +0200

    small fixes

--- code/main.py
+++ code/main.py
@@ -31,7 +31,7 @@ completed_files = [line for line in completed_txt_file.readlines()]
 completed_files = [file.replace('\n', '') for file in completed_files]
 
 files = [f for f in files if f not in completed_files]
-files = files[0:1]
+
 open(new_processed_files_path, "w").close()
 
 if files:
