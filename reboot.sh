2a3f5d9c17605407b275faaf580b8a1e2cd77110
Joakim Eriksson <90309369+aidotsejoakim@users.noreply.github.com>
2024-04-15 11:15:37 +0200

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -13,6 +13,6 @@ params_to_update="#env_params.temperature=25"
 
 # run python scripts
 /home/joakim/Dokument/git/echoedge/venv/bin/python3.11 /home/joakim/Dokument/git/echoedge/code/send_ready.py "$serial_path"
-# /home/joakim/Dokument/git/echoedge/venv/bin/python3.11 /home/joakim/Dokument/git/echoedge/code/update_params.py "$params_path" "$params_ranges_path" "$params_to_update"
+/home/joakim/Dokument/git/echoedge/venv/bin/python3.11 /home/joakim/Dokument/git/echoedge/code/update_params.py "$params_path" "$params_ranges_path" "$params_to_update" "$serial_path"
 /home/joakim/Dokument/git/echoedge/venv/bin/python3.11 /home/joakim/Dokument/git/echoedge/code/main.py "$raw_path" "$completed_files_path" "$new_files_path" "$csv_path" "$params_path" "$img_path"
 /home/joakim/Dokument/git/echoedge/venv/bin/python3.11 /home/joakim/Dokument/git/echoedge/code/send_results.py "$csv_path" "$new_files_path" "$serial_path"
